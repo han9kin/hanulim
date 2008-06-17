2687f4908f59a5af295b1143b9c6a9b8b67dd1d2
Sanghyuk Suh <han9kin@mac.com>
2008-06-17 19:23:27 +0000

    issue 4

--- Component/HIMMethod.c
+++ Component/HIMMethod.c
@@ -8,15 +8,16 @@
 
 
 #define KIND(code)  (code >> 8)
-#define VALUE(code) ((SInt8)code)
+#define VALUE(code) ((UInt8)code)
 
 
 enum
 {
-    kKindSymbol  = 0,
-    kKindInitial = 1,
-    kKindMedial  = 2,
-    kKindFinal   = 3
+    kKindSymbol    = 0,
+    kKindInitial   = 1,
+    kKindMedial    = 2,
+    kKindFinal     = 3,
+    kKindDiacritic = 4
 };
 
 
@@ -32,7 +33,7 @@ UInt16 HIMKeyCode(UInt32 aKeyCode, Boolean aShiftFlag)
         sKind  = KIND(sCode);
         sValue = VALUE(sCode);
 
-        if ((sKind == kKindSymbol) && ((sValue > gSymbolMax) || (gSymbol[sValue] == 0)))
+        if ((sKind == kKindSymbol) && ((sValue > HNUnicodeSymbolMax) || (HNUnicodeSymbol[sValue] == 0)))
         {
             return 0;
         }
@@ -117,7 +118,7 @@ Boolean HIMHandleKey(HIMSessionHandle aSessionHandle,
     {
         if (KIND(sCode) == kKindSymbol)
         {
-            sChar = gSymbol[VALUE(sCode)];
+            sChar = HNUnicodeSymbol[VALUE(sCode)];
 
             if (gPreferences.mSmartQuotationMarks)
             {
@@ -177,70 +178,74 @@ Boolean HIMHandleKey(HIMSessionHandle aSessionHandle,
     }
 }
 
-SInt8 HIMCombine(UInt8 aKind, SInt8 aValue1, SInt8 aValue2)
+UInt8 HIMCombine(UInt8 aKind, UInt8 aValue1, UInt8 aValue2)
 {
-    UInt16 sFactor;
-    UInt32 sCount;
-    UInt32 i;
-
-    sCount  = gCompoundCount[aKind - 1][HIMArchaicKeyboard()];
-    sFactor = (aValue1 << 8) | aValue2;
-
-    for (i = 0; i < sCount; i++)
+    if ((aKind >= kKindInitial) && (aKind <= kKindFinal))
     {
-        if (gCompoundFactor[aKind - 1][i] == sFactor)
+        HNJasoComposition *sTable = &HNJasoCompositionTable[aKind];
+        UInt16             sFactor;
+        UInt32             sCount;
+        UInt32             i;
+
+        sCount  = sTable->mCount[HIMArchaicKeyboard() ? 1 : 0];
+        sFactor = (aValue1 << 8) | aValue2;
+
+        for (i = 0; i < sCount; i++)
         {
-            return gCompoundResult[aKind - 1][i];
+            if (sTable->mIn[i] == sFactor)
+            {
+                return sTable->mOut[i];
+            }
         }
     }
 
-    return -1;
+    return (UInt8)-1;
 }
 
-void HIMBufferClear(SInt8 *aBuffer)
+void HIMBufferClear(UInt8 *aBuffer)
 {
     aBuffer[0] = 0;
-    aBuffer[1] = -1;
-    aBuffer[2] = -1;
-    aBuffer[3] = -1;
+    aBuffer[1] = (UInt8)-1;
+    aBuffer[2] = (UInt8)-1;
+    aBuffer[3] = (UInt8)-1;
+    aBuffer[4] = (UInt8)-1;
 }
 
-void HIMBufferCopy(SInt8 *aSrcBuf, SInt8 *aDestBuf)
+void HIMBufferCopy(UInt8 *aSrcBuf, UInt8 *aDestBuf)
 {
     aDestBuf[0] = aSrcBuf[0];
     aDestBuf[1] = aSrcBuf[1];
     aDestBuf[2] = aSrcBuf[2];
     aDestBuf[3] = aSrcBuf[3];
+    aDestBuf[4] = aSrcBuf[4];
 }
 
-void HIMBufferSet(SInt8 *aBuffer, UInt8 aKind, SInt8 aValue)
+void HIMBufferSet(UInt8 *aBuffer, UInt8 aKind, UInt8 aValue)
 {
     aBuffer[0]     = aKind;
     aBuffer[aKind] = aValue;
 }
 
-UInt32 HIMGenerateChar(SInt8 *aBuffer, UniChar *aChar)
+UInt32 HIMGenerateChar(UInt8 *aBuffer, UniChar *aChar)
 {
-    static const SInt8   sMin[4]    = { 0, 0, 0, 1 };
-    static const SInt8   sCMax[4]   = { 0, 89, 65, 82 };
-    static const SInt8   sSMax[4]   = { 0, 18, 20, 27 };
-    static const UniChar sBase[4]   = { 0, 0x1100, 0x1161, 0x11a7 };
-    static const UniChar sFiller[4] = { 0, 0x115f, 0x1160, 0 };
-    UInt32 sLength                  = 0;
-    UInt32 i;
+    static const UInt8   sMaxNFC[]    = { 0x00, 0x13, 0x15, 0x1b };
+    static const UInt8   sMaxNFD[]    = { 0x00, 0x7c, 0x5e, 0x89 };
+    static const UniChar sDiacritic[] = { 0x0000, 0x302e, 0x302f };
+    UInt32               sLength      = 0;
+    UInt32               i;
 
     if (HIMInputConjoiningJamo())
     {
         for (i = kKindInitial; i <= kKindFinal; i++)
         {
-            if ((aBuffer[i] >= sMin[i]) && (aBuffer[i] <= sCMax[i]))
+            if (aBuffer[i] <= sMaxNFD[i])
             {
-                aChar[i - 1] = sBase[i] + aBuffer[i];
+                aChar[i - 1] = HNUnicodeJaso[i][aBuffer[i]];
                 sLength = i;
             }
             else
             {
-                aChar[i - 1] = sFiller[i];
+                aChar[i - 1] = HNUnicodeJaso[i][0];
             }
         }
 
@@ -248,68 +253,79 @@ UInt32 HIMGenerateChar(SInt8 *aBuffer, UniChar *aChar)
         {
             sLength = 2;
         }
+
+        if ((aBuffer[kKindDiacritic] > 0) && (aBuffer[kKindDiacritic] <= 2))
+        {
+            if (sLength < 2)
+            {
+                sLength = 2;
+            }
+
+            aChar[sLength] = sDiacritic[aBuffer[kKindDiacritic]];
+            sLength++;
+        }
     }
     else
     {
-        SInt8 sBuffer[4];
+        UInt8 sBuffer[5];
         UInt8 sCount = 0;
         UInt8 sKind  = 0;
 
         HIMBufferCopy(aBuffer, sBuffer);
 
-        if (sBuffer[3] < 0)
-        {
-            sBuffer[3] = 0;
-        }
-
-        sLength = 1;
-
         for (i = kKindInitial; i <= kKindFinal; i++)
         {
-            if ((sBuffer[i] >= sMin[i]) && (sBuffer[i] <= sSMax[i]))
+            if (sBuffer[i] <= sMaxNFC[i])
             {
                 sCount++;
                 sKind = i;
             }
         }
 
-        if (sCount == ((sKind == kKindFinal) ? 3 : 2))
+        if (sCount == 1)
         {
-            int sIndex = (sBuffer[kKindInitial] * 21 * 28) + (sBuffer[kKindMedial] * 28) + sBuffer[kKindFinal];
-            if (HIMInputDocumentHasProperty(kTSMDocumentUnicodePropertyTag) ||
-                CanBeConvertedToKSC(sIndex))
+            *aChar  = HNUnicodeJamo[sKind][sBuffer[sKind]];
+            sLength = 1;
+        }
+        else
+        {
+            int sIndex = -1;
+
+            if (sCount == 3)
             {
-                *aChar = 0xac00 + sIndex;
+                sIndex = ((sBuffer[kKindInitial] - 1) * 21 * 28) + ((sBuffer[kKindMedial] - 1) * 28) + sBuffer[kKindFinal];
+            }
+            else if ((sCount == 2) && (sKind < kKindFinal))
+            {
+                sIndex = ((sBuffer[kKindInitial] - 1) * 21 * 28) + ((sBuffer[kKindMedial] - 1) * 28);
+            }
+
+            if ((sIndex > -1) && (HIMInputDocumentHasProperty(kTSMDocumentUnicodePropertyTag) || CanBeConvertedToKSC(sIndex)))
+            {
+                *aChar  = 0xac00 + sIndex;
+                sLength = 1;
             }
             else
             {
                 sLength = 0;
             }
         }
-        else if (sCount == 1)
-        {
-            *aChar = gCompatibilityJamo[sKind - 1][sBuffer[sKind]];
-        }
-        else
-        {
-            sLength = 0;
-        }
     }
 
     return sLength;
 }
 
-UInt32 HIMCompositeBuffer(SInt8 *aBuffer, UInt8 aKind, SInt8 aValue, UniChar *aChar)
+UInt32 HIMCompositeBuffer(UInt8 *aBuffer, UInt8 aKind, UInt8 aValue, UniChar *aChar)
 {
     UInt32 sLength;
 
     if (aBuffer[0] <= aKind)
     {
-        SInt8 sNewValue = (aBuffer[aKind] > -1) ? HIMCombine(aKind, aBuffer[aKind], aValue) : aValue;
+        UInt8 sNewValue = (aBuffer[aKind] != (UInt8)-1) ? HIMCombine(aKind, aBuffer[aKind], aValue) : aValue;
 
-        if (sNewValue > -1)
+        if (sNewValue != (UInt8)-1)
         {
-            SInt8 sTestBuf[4];
+            UInt8 sTestBuf[5];
 
             HIMBufferCopy(aBuffer, sTestBuf);
             HIMBufferSet(sTestBuf, aKind, sNewValue);
@@ -345,7 +361,8 @@ void HIMDequeueKeyBuffer(HIMSessionHandle aSessionHandle, UInt32 aCount)
 void HIMComposite(HIMSessionHandle aSessionHandle)
 {
     UniCharPtr sCharBuffer = (*aSessionHandle)->mCharBuffer;
-    SInt8      sBuffer[4]  = {0, -1, -1, -1};
+    UniCharPtr sCharLimit  = sCharBuffer + (kBufferMax - 5);
+    UInt8      sBuffer[5]  = {0, (UInt8)-1, (UInt8)-1, (UInt8)-1, (UInt8)-1};
     UInt32     sLength;
     UInt32     i;
 
@@ -355,40 +372,40 @@ void HIMComposite(HIMSessionHandle aSessionHandle)
     {
         UInt16 sCode  = (*aSessionHandle)->mKeyBuffer[i];
         UInt8  sKind  = KIND(sCode);
-        SInt8  sValue = VALUE(sCode);
+        UInt8  sValue = VALUE(sCode);
 
         if (HIMOverloadConsonants())
         {
             if ((sKind == kKindInitial) && (sBuffer[0] > sKind))
             {
-                SInt8 sFinal = gInitialToFinal[sValue];
+                UInt8 sFinal = HNJasoInitialToFinal[sValue];
 
                 if (sFinal &&
-                    ((sBuffer[kKindFinal] == -1) ||
+                    ((sBuffer[kKindFinal] == (UInt8)-1) ||
                      ((sBuffer[kKindFinal] != sFinal) &&
-                      ((sFinal = HIMCombine(kKindFinal, sBuffer[kKindFinal], sFinal)) > -1))))
+                      ((sFinal = HIMCombine(kKindFinal, sBuffer[kKindFinal], sFinal)) != (UInt8)-1))))
                 {
-                    SInt8 sTestBuf[4];
+                    UInt8 sTestBuf[5];
 
                     HIMBufferCopy(sBuffer, sTestBuf);
                     HIMBufferSet(sTestBuf, kKindFinal, sFinal);
 
                     if (HIMGenerateChar(sTestBuf, sCharBuffer))
                     {
-                        sKind = kKindFinal;
-                        sValue = gInitialToFinal[sValue];
+                        sKind  = kKindFinal;
+                        sValue = HNJasoInitialToFinal[sValue];
                     }
                 }
             }
             else if ((sKind == kKindMedial) && (sBuffer[0] == kKindFinal))
             {
-                SInt8 sInitial;
-                SInt8 sFinal;
+                UInt8 sInitial;
+                UInt8 sFinal;
 
                 sInitial = VALUE((*aSessionHandle)->mKeyBuffer[i - 1]);
                 sFinal   = ((i > 1) && (KIND((*aSessionHandle)->mKeyBuffer[i - 2]) == kKindInitial)) ?
-                    gInitialToFinal[VALUE((*aSessionHandle)->mKeyBuffer[i - 2])] :
-                    -1;
+                    HNJasoInitialToFinal[VALUE((*aSessionHandle)->mKeyBuffer[i - 2])] :
+                    (UInt8)-1;
 
                 sBuffer[kKindFinal] = sFinal;
                 sLength             = HIMGenerateChar(sBuffer, sCharBuffer);
@@ -406,6 +423,12 @@ void HIMComposite(HIMSessionHandle aSessionHandle)
                 {
                     (*aSessionHandle)->mCharBufferCount += sLength;
                     sCharBuffer += sLength;
+
+                    if (sCharBuffer > sCharLimit)
+                    {
+                        fprintf(stderr, "HANULIM ERROR: Buffer overflow. composition stopped.\n");
+                        break;
+                    }
                 }
 
                 HIMBufferClear(sBuffer);
@@ -430,6 +453,12 @@ void HIMComposite(HIMSessionHandle aSessionHandle)
             {
                 (*aSessionHandle)->mCharBufferCount += sLength;
                 sCharBuffer += sLength;
+
+                if (sCharBuffer > sCharLimit)
+                {
+                    fprintf(stderr, "HANULIM ERROR: Buffer overflow. composition stopped.\n");
+                    break;
+                }
             }
         }
     }
